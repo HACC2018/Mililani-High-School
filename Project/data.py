8771ccefd75e5d39ce0020d661634c25f0ef0de7
Jason <jgodmere808@gmail.com>
2018-10-25 18:37:47 -1000

    Git commands, intitial skeleton of application

---
+++ Project/data.py
@@ -0,0 +1,21 @@
+class DataPoint:
+
+    def __init__(self, date, time, kilowatts):
+        self.date = date
+        self.time = time
+        self.kilowatts = kilowatts
+
+
+class Building(DataPoint):
+
+    def __init__(self, name, dataPoints):
+        self.name = name
+        self.dataPoints = dataPoints
+
+
+class Campus(Building):
+
+    def __init__(self, name, buildings):
+        self.name = name
+        self.buildings = buildings
+
---
+++ Project/main.py
@@ -0,0 +1,10 @@
+import data
+
+class Borealis(data.Campus):
+
+    def __init__(self):
+        pass
+
+
+    def LoadData(self):
+    	pass
