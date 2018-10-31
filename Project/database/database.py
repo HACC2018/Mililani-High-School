b0b72dba5b63c912a8000072134be10a26cec175
Shane Parslow <44428643+ShaneParslow@users.noreply.github.com>
2018-10-30 17:37:32 -1000

    Revert "Shane"

--- Project/database/database.py
+++ Project/database/database.py
@@ -5,17 +5,8 @@ class Database():
 #The database class will be re-initialized every 
 #time we change its parameters in the main application
 	def __init__(self):
-		pass
-	def readCSV(self, csv, startTime, endTime)
-		pass
+		print("Hello World")
+
 
 #initializes an instance of database
 database = Database()
-
-
-'''
-time format:
-yymmddhhmm
-recorded on whole-minute intervals
-24 hour time
-'''
