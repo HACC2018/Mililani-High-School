5d70dcb5031344f46db2cb91c1430d00d91d672d
Jason <jgodmere808@gmail.com>
2018-11-01 12:46:56 -1000

    Jason. Added timestamp handling to the database class

--- Project/database/csvTest.py
+++ Project/database/csvTest.py
@@ -5,3 +5,4 @@ with open('AnalyticsData_20181019174047.csv') as csvData: #Open test CSV as obje
     data = list(csvReader) #Cast CSV reader output to 2d list
     buildingNum = len(data[0]) - 1 #Take length of 2d array row and subtract one (for time collumn) to find # of buildings
     print(buildingNum)
+
