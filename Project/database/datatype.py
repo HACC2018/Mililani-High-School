977c1f4d139f1e738cbb15d02647d557c859de3a
Shane Parslow <shaneparslow808@gmail.com>
2018-11-03 16:45:23 -1000

    add CSV class to datatype

--- Project/database/datatype.py
+++ Project/database/datatype.py
@@ -2,12 +2,16 @@ import matplotlib.pyplot as plt
 
 class DataPoint:
 
-    def __init__(self, timestamp, kilowatts):#store timestamp as a unix timestamp
-        self.timestamp = timestamp
-        self.kilowatts = kilowatts
+	def __init__(self, timestamp, kilowatts):#store timestamp as a unix timestamp
+		self.timestamp = timestamp
+		self.kilowatts = kilowatts
 
 
 class Building:
 
-    def __init__(self):
-        self.dataPoints = []
+	def __init__(self):
+		self.dataPoints = []
+
+class CSV:
+	def __init__(self, path)
+		pass
