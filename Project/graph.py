7df1fbc59c20490800e35670b1b9609c18188427
Jason <jgodmere808@gmail.com>
2018-11-03 19:54:26 -1000

    working graph, need to fix legend and change x label

--- Project/graph.py
+++ Project/graph.py
@@ -1,42 +1,37 @@
 #import matplotlib here
 
 import matplotlib.pyplot as plt
-#import database.database as database 
+import database.database as data
 
 #List of coordinates for plotting 
 
 class Graph:
 
-	def __init__(self): 
-		plt.plot()
-		plt.show()
-
-	def setInterval(self, start, end):
-		database.setInterval
-
-
-
-
-
-	'''def __init__(self):
-		self.timeStamps = []
-		self.kilowatts = []
+	def __init__(self, database):
 		#Initialize graph after prospects 
 		#Loop for grabbing buildings and data points 
-		for i in database.buildingData:
-			for j in database.buildingData[i].dataPoints[j]:
-				self.timeStamps.append(database.buildingData[i].dataPoints[j].timestamp)
-				self.kilowatts.append(database.buildingData[i].dataPoints[j].kilowatts)
+		for buildingNum in range(0, len(database.buildingsData)):
+			self.timestamps = []
+			self.kilowatts = []
+			for dataPoint in range(0, len(database.buildingsData[buildingNum].dataPoints)):
+				self.timestamps.append(database.SetDateToUnix(database.buildingsData[buildingNum].dataPoints[dataPoint].timestamp))
+				self.kilowatts.append(float(database.buildingsData[buildingNum].dataPoints[dataPoint].kilowatts))
 
 			#plt.xticks(ticks= , labels=, rotation=45)
-			plt.plot(self.timeStamps,self.kilowatts,label=str(database.buildingData[i].name))
-			plt.xlabel('Date/Time')
+			plt.plot(self.timestamps,self.kilowatts,label=str(database.buildingsData[buildingNum].name))
+			plt.xlabel('Timestamp')
 			plt.ylabel('Kilowatts')
-			plt.title('Buildings')
+			plt.title(database.buildingsData[buildingNum].name)
 			plt.legend()
-			plt.show()
+		plt.show()
+ 
+database = data.Database('database/csv/AnalyticsData_20181019174047.csv')#test csv
+#building = Building(buildings[0])
 
-'''
+database.SetInterval(" 1/3/2018 10:30:00 AM", " 1/10/2018 10:30:00 AM")
+for i in range(5, len(database.buildings)):
+	database.AddBuilding(i)
 
-graph = Graph()
+database.ReadData()
+graph = Graph(database)
 
